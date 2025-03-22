5b6129dc99acb688192945f456e3aa2536a98154
양승권 <105967323+ysk102706@users.noreply.github.com>
2025-03-22 11:13:52 +0900

    공격 충돌 시스템 틀 완성

---
+++ Source/RedSoul/Interface/Hitable.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Hitable.generated.h" 
+
+USTRUCT(BlueprintType)
+struct FAttackInfo {
+	GENERATED_USTRUCT_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Damage;
+};
+
+UINTERFACE(MinimalAPI)
+class UHitable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class REDSOUL_API IHitable
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION()
+	virtual void Hit(FAttackInfo AttackInfo) = 0;
+
+};
